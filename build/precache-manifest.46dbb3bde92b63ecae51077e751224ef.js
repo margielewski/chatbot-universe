7fe972940e7a251039821e349c4d78f141776ad3
Stanisław <stanislaw.margielewski@gmail.com>
2019-09-13 14:44:44 +0200

    build app

---
+++ build/precache-manifest.46dbb3bde92b63ecae51077e751224ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0b319a946862ddc28e10541e3d8883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb8def5dd4008289dfbb",
+    "url": "/static/css/main.9291ebe8.chunk.css"
+  },
+  {
+    "revision": "61d4f67e578db343fdad",
+    "url": "/static/js/2.3ee54845.chunk.js"
+  },
+  {
+    "revision": "eb8def5dd4008289dfbb",
+    "url": "/static/js/main.b6aa28dd.chunk.js"
+  },
+  {
+    "revision": "07af7980f5535193c380",
+    "url": "/static/js/runtime~main.d4554acb.js"
+  },
+  {
+    "revision": "9a8342d520397ffb2251afe23c6f55f5",
+    "url": "/static/media/MM_ikona-1.9a8342d5.png"
+  },
+  {
+    "revision": "da00eaced67068c8c4772ceba9026bf1",
+    "url": "/static/media/bg.da00eace.jpg"
+  },
+  {
+    "revision": "399bba3345e409041c5136867b7c4e09",
+    "url": "/static/media/iPhone-6S-Space-Gray-copy.399bba33.png"
+  }
+]);
