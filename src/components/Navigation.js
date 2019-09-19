5172d2173099d1daae73d9cbc954b51067fc7242
Stanisław <stanislaw.margielewski@gmail.com>
2019-09-19 12:38:32 +0200

    fix fetch api in components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,14 +8,13 @@ class Navigation extends Component {
     }
     componentDidMount() {
         let url = 'http://chatbotest.xyz:1337/Navigations';
-        fetch(url).
-            then(response => response.json()).then((repos) => {
+        fetch(url).then(response => response.json()).then((repos) => {
 
-                this.setState({
-                    content: repos
-                });
-
+            this.setState({
+                content: repos
             });
+
+        });
 
     }
 
@@ -27,68 +26,69 @@ class Navigation extends Component {
         } else {
             const { content } = this.state;
             const arr = content.map(i => i.title);
+
+            console.log(arr)
+
             return (
+                <>
+                    <section className='navigation'>
+                        <div className='container'>
+                            <div className='row'>
+                                <div className='col-5'>
+                                    <div className='navigation__logo'>
+                                        <div className='logo__image'>
+                                            <Link
+                                                activeClass="active"
+                                                to="Choice"
+                                                spy={true}
+                                                smooth={true}
+                                                offset={-125}
+                                                duration={500}
+                                            >
+                                                <img className='image-size' alt="Logo" src={logoIkonka}>
+                                                </img>
+                                                <span className="logo__text">Chatbot Universe</span></Link>
 
 
-        return (
-            <>
-                <section className='navigation'>
-                    <div className='container'>
-                        <div className='row'>
-                            <div className='col-5'>
-                                <div className='navigation__logo'>
-                                    <div className='logo__image'>
-                                        <Link
-                                            activeClass="active"
-                                            to="Choice"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-125}
-                                            duration={500}
-                                        >
-                                            <img className='image-size' alt="Logo" src={logoIkonka}>
-                                            </img>
-                                        </Link>
-
-
+                                        </div>
                                     </div>
+
                                 </div>
+                                <div className='col-7 navigation__nav'>
+                                    <ul className='nav-list'>
+                                        <li className='nav__item'>
+                                            <Link
+                                                activeClass="active"
+                                                to="MMdE"
+                                                spy={true}
+                                                smooth={true}
+                                                offset={-125}
+                                                duration={500}
+                                            >{arr[0]}</Link>
+                                        </li>
+                                        <li className='nav__item'>
+                                            <Link
+                                                activeClass="active"
+                                                to="Chatbot"
+                                                spy={true}
+                                                smooth={true}
+                                                offset={-125}
+                                                duration={500}
+                                            >{arr[1]}</Link>
+                                        </li>
+                                        <li className='nav__item'>
+                                            <Link
+                                                activeClass="active"
+                                                to="Contact"
+                                                spy={true}
+                                                smooth={true}
+                                                offset={-125}
+                                                duration={500}
+                                            >{arr[2]}</Link>
+                                        </li>
+                                    </ul>
 
-                            </div>
-                            <div className='col-7 navigation__nav'>
-                                <ul className='nav-list'>
-                                    <li className='nav__item'>
-                                        <Link
-                                            activeClass="active"
-                                            to="MMdE"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-125}
-                                            duration={500}
-                                        >Messenger Marketing</Link>
-                                    </li>
-                                    <li className='nav__item'>
-                                        <Link
-                                            activeClass="active"
-                                            to="Chatbot"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-125}
-                                            duration={500}
-                                        >Chatbot</Link>
-                                    </li>
-                                    <li className='nav__item'>
-                                        <Link
-                                            activeClass="active"
-                                            to="Contact"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-125}
-                                            duration={500}
-                                        >Kontakt</Link>
-                                    </li>
-                                </ul>
-
+                                </div>
                             </div>
                         </div>
                     </section>
