0d3fd3c1b91a4ae3666c24ea579040a4c77d9704
Stanisław <stanislaw.margielewski@gmail.com>
2019-09-24 12:48:08 +0200

    fix annimations

--- src/components/ButtonShowLessC.js
+++ src/components/ButtonShowLessC.js
@@ -5,26 +5,27 @@ class ButtonShowLessC extends Component {
     render() {
         // console.log(this.props.display)
         return (
-            <>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-12'>
-                            <section className='ButtonShowLess'>
-                                <div className='row'>
-                                    <div className='col-12'>
-                                        <div className='ButtonShowLess__text'>
-                                            <button className='text__button' onClick={this.props.clickSelectedChatbot.bind(null, false)}>Zobacz mniej</button>
-
-                                        </div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <section className='ButtonShowLess'>
+                            <div className='row'>
+                                <div className='col-12'>
+                                    <div className='ButtonShowLess__text'>
+                                        <button className='text__button' onClick={() => displayGreeting(a => {
+                                            console.log(a)
+                                            return false
+                                        })}>Zobacz mniej</button>
 
                                     </div>
-                                </div>
 
-                            </section>
-                        </div>
+                                </div>
+                            </div>
+
+                        </section>
                     </div>
                 </div>
-            </>
+            </div>
         )
     }
 }
